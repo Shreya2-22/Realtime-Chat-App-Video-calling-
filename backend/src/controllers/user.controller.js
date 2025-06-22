47cfeb82ed944c964c1f689f11d7ca1bd18b5814
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-22 23:09:25 +0545

    create send friend request endpoint

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,3 +1,5 @@
+import FriendRequest from "../models/FriendRequest.js";
+
 export async function getRecommendedUsers(req, res) {
     try{
         const currentUserId = req.user.id;
@@ -25,6 +27,49 @@ export async function getMyFriends(req, res) {
         res
     }catch(error){
         console.error("Error in getMyFriends:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export async function sendFriendRequest(req, res) {
+    try{
+        const myId = req.user.id;
+        const {id: recipientId} = req.params;
+        //prevent sending friend request to self
+        if(myId === recipientId) {
+            return res.status(400).json({message: "You cannot send a friend request to yourself"});
+        }
+
+        const recipient = await User.findById(recipientId);
+        if(!recipient) {
+            return res.status(404).json({message: "Recipient not found"});
+        }
+
+        //check is user is already friends with recipient
+        if(recipient.friends.includes(myId)) {
+            return res.status(400).json({message: "You are already friends with this user"});
+        }
+
+        //check is request already exists
+        const existingRequest = await FriendRequest.findOne({
+            $or: [
+                { sender: myId, recipient: recipientId },
+                { sender: recipientId, recipient: myId }
+            ],
+        });
+        if(existingRequest) {
+            return res.status(400).json({message: "Friend request already exists between you and this user"});
+        }
+
+        //create new friend request
+        const friendRequest = await FriendRequest.create({
+            sender: myId,
+            recipient: recipientId,
+        });
+        res.status(201).json(friendRequest);
+        
+    }catch(error){
+        console.error("Error in sendFriendRequest:", error);
         res.status(500).json({message: "Internal server error"});
     }
 }
