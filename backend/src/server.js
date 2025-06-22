f97a76e8c418d3bbf2c061554a05f2c10b841ead
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-22 22:28:22 +0545

    protect route and onboarding endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import "dotenv/config";
 import authRoutes from './routes/auth.route.js'
 import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 const app=express()
 const PORT= process.env.PORT;
+app.use(cookieParser()); // Middleware to parse cookies
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use("/api/auth", authRoutes);
+
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
