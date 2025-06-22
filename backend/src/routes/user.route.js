7387a59afa38becd0e9b8b02ea8265f8d3be5d6d
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-22 23:31:23 +0545

    complete user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest } from '../controllers/user.controller.js';
+import { getRecommendedUsers, getMyFriends, sendFriendRequest, acceptFriendRequest, getFriendRequests, getOutgoingFriendRequests } from '../controllers/user.controller.js';
 
 const router = express.Router();
 router.use(protectRoute);
@@ -9,5 +9,7 @@ router.get('/',getRecommendedUsers);
 router.get('/friends',getMyFriends);
 router.post('/friend-request/:id', sendFriendRequest);
 router.put('/friend-request/:id/accept', acceptFriendRequest);
+router.get('/friend-requests', getFriendRequests);
+router.get('/outgoing-friend-requests', getOutgoingFriendRequests);
 
 export default router;
