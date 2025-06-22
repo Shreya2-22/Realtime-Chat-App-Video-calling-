5e6d445d08b9b733e7dd5f058568c168d1410f67
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-22 10:54:00 +0545

    optimize codebase

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export async function signup(req, res) {
+    // Logic for user signup
+    res.send('Signup Route');
+}
+
+export async function login(req, res) {
+    // Logic for user login
+    res.send('Login Route');
+}
+
+export function logout(req, res) {
+    // Logic for user logout
+    res.send('Logout Route');
+}
