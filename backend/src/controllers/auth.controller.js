7704850be01946c462867e4859d401a1768cc69c
Shreya2-22 <kshreya22@tbc.edu.np>
2025-06-22 21:12:48 +0545

    signup endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,53 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+
 export async function signup(req, res) {
-    // Logic for user signup
-    res.send('Signup Route');
+    const {fullName, email, password} = req.body;
+    try{
+        if(!email || !password || !fullName){
+            return res.status(400).json({message: "Please fill all the fields"});
+        }
+
+        if(password.length<6){
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
+        }
+
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)){
+            return res.status(400).json({message: "Please enter a valid email address"});
+        }
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: "User with this email already exists"});
+        }
+
+        const idx = Math.floor(Math.random() * 100)+1; //generate a random number between 1 and 100
+        const randomAvatar = `https://avatar-placeholder.iran.liara.run/public/${idx}.png`; //use the random number to get a random avatar
+        
+        const newUser = await User.create({
+            fullName,
+            email,
+            password,
+            profilePic: randomAvatar,
+        });
+        //create the user in stream as well
+        const token = jwt.sign({userId: newUser._id,},
+             process.env.JWT_SECRET_KEY, 
+             {expiresIn: '7d'});
+
+        res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: process.env.NODE_ENV === 'production', // Set to true in production
+        });
+
+        res.status(201).json({success: true, user: newUser});
+    }catch(error){
+        console.error("Error in signup:", error);
+        res.status(500).json({message: "Internal server error"});
+    }
 }
 
 export async function login(req, res) {
